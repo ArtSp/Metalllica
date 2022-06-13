2408467ed4e2c3abbda40349742d522272422aa2
Artjoms Spole <artjoms.spole@gmail.com>
2022-06-13 08:51:32 +0300

    Initial Commit

---
+++ Metallica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Metallica
+//
+//  Created by Artjoms Spole on 13/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metallica/MetallicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetallicaApp.swift
+//  Metallica
+//
+//  Created by Artjoms Spole on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MetallicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
